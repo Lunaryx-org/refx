7bcd01223398d3f19ab0eac43df7725afb84dd28
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-10-12 08:08:33 +0200

    pushed: v0.3.1

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCMD = &cobra.Command{
 	Use:   "version",
 	Short: "print the version of the app",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.2.0")
+		fmt.Println("0.3.1")
 	},
 }
 
